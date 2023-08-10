784e2798c67a524119e3fdfc7b0dc7ed532804b2
SBTheara <thearasambath112@gmail.com>
2023-08-10 16:25:14 +0700

    can post but can't get

--- src/main/java/com/oneone/oneone/controller/EmployeeController.java
+++ src/main/java/com/oneone/oneone/controller/EmployeeController.java
@@ -1,9 +1,8 @@
 package com.oneone.oneone.controller;
-
-import com.oneone.oneone.model.Address;
 import com.oneone.oneone.model.Employee;
 import com.oneone.oneone.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,9 +18,8 @@ public class EmployeeController {
         CommentRepository.saveAll(emp);
         return ResponseEntity.ok("Success");
     }
-    @GetMapping("/getEmployees")
-    public List<Employee> getEmployees(){
-        return CommentRepository.findAll();
+    @GetMapping("/get")
+    public ResponseEntity<List<Employee>> getdata(){
+        return new ResponseEntity<>(CommentRepository.findAll(), HttpStatus.OK);
     }
-
 }
